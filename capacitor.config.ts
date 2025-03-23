605a022bc2e5bf38cd7444ee4bef60dead0a90c8
Benjamin Haruna Bala <benjaminharuna099@gmail.com>
2025-03-23 04:15:51 -1000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionicTabs',
+  webDir: 'dist'
+};
+
+export default config;
